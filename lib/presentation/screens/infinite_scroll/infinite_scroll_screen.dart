fe8d032e6827c0c41fd450555d1a14526ea24040
amandabrodriguez <amandabreyesr@gmail.com>
2023-05-16 09:20:52 -0600

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScroll extends StatelessWidget {
+  const InfiniteScroll({super.key});
+
+  static const name = 'infinite_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiControlScreen extends StatelessWidget {
+  const UiControlScreen({super.key});
+
+  static const name = 'ui_control_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
