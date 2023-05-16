03233d27f62fe8a07dcc95fd564d29602866dd15
amandabrodriguez <amandabreyesr@gmail.com>
2023-05-16 15:23:19 -0600

    ui screen finished

--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -7,6 +7,128 @@ class UiControlScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls'),
+      ),
+      body: const _UIControlsView(),
+    );
+  }
+}
+
+enum Transportation {
+  car,
+  plain,
+  boat,
+  submarine,
+}
+
+class _UIControlsView extends StatefulWidget {
+  const _UIControlsView();
+
+  @override
+  State<_UIControlsView> createState() => _UIControlsViewState();
+}
+
+class _UIControlsViewState extends State<_UIControlsView> {
+  bool switchValue = false;
+  Transportation selectedTransportation = Transportation.car;
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: switchValue,
+          onChanged: (value) {
+            switchValue = value;
+            setState(() {});
+          },
+        ),
+        ExpansionTile(
+          title: const Text('Vehículo de transporte'),
+          subtitle: Text(selectedTransportation.name),
+          children: [
+            _CustomRadioListTile(
+              transportationValue: Transportation.car,
+              selectedTransportation: selectedTransportation,
+              onPressed: () => setState(() {
+                selectedTransportation = Transportation.car;
+              }),
+            ),
+            _CustomRadioListTile(
+              transportationValue: Transportation.plain,
+              selectedTransportation: selectedTransportation,
+              onPressed: () => setState(() {
+                selectedTransportation = Transportation.plain;
+              }),
+            ),
+            _CustomRadioListTile(
+              transportationValue: Transportation.boat,
+              selectedTransportation: selectedTransportation,
+              onPressed: () => setState(() {
+                selectedTransportation = Transportation.boat;
+              }),
+            ),
+            _CustomRadioListTile(
+              transportationValue: Transportation.submarine,
+              selectedTransportation: selectedTransportation,
+              onPressed: () => setState(() {
+                selectedTransportation = Transportation.submarine;
+              }),
+            ),
+          ],
+        ),
+        CheckboxListTile(
+          value: wantsBreakfast,
+          title: const Text('¿Desayuno?'),
+          onChanged: (value) => setState(() {
+            wantsBreakfast = !wantsBreakfast;
+          }),
+        ),
+        CheckboxListTile(
+          value: wantsLunch,
+          title: const Text('¿Almuerzo?'),
+          onChanged: (value) => setState(() {
+            wantsLunch = !wantsLunch;
+          }),
+        ),
+        CheckboxListTile(
+          value: wantsDinner,
+          title: const Text('¿Cena?'),
+          onChanged: (value) => setState(() {
+            wantsDinner = !wantsDinner;
+          }),
+        ),
+      ],
+    );
+  }
+}
+
+class _CustomRadioListTile extends StatelessWidget {
+  const _CustomRadioListTile(
+      {required this.onPressed,
+      required this.transportationValue,
+      required this.selectedTransportation});
+
+  final Function() onPressed;
+  final Transportation transportationValue;
+  final Transportation selectedTransportation;
+
+  @override
+  Widget build(BuildContext context) {
+    return RadioListTile(
+      title: Text('By ${transportationValue.name.toString()}'),
+      subtitle: Text('Travel by ${transportationValue.name}'),
+      value: transportationValue,
+      groupValue: selectedTransportation,
+      onChanged: (value) => onPressed(),
+    );
   }
 }
